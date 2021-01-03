6a19b4ce8d4e7ae7fee639c3cc05a84e7b5e8612
André Formento <andreformento.sc@gmail.com>
2021-01-03 20:09:41 -0300

    refactor backend

---
+++ spring-social/src/main/java/com/example/springsocial/auth/rest/AuthController.java
@@ -0,0 +1,48 @@
+package com.example.springsocial.auth.rest;
+
+import com.example.springsocial.user.UserRepository;
+import com.example.springsocial.auth.security.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = tokenProvider.createToken(authentication);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+}
---
+++ spring-social/src/main/java/com/example/springsocial/auth/rest/AuthProvider.java
@@ -0,0 +1,7 @@
+package com.example.springsocial.auth.rest;
+
+public enum  AuthProvider {
+    local,
+    facebook,
+    google
+}
